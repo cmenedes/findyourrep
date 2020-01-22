e3f2a865bffae7bded37bad95de6e7374617e4be
Christopher Menedes <cmenedes@yahoo.com>
2020-01-21 19:32:51 -0500

    template for Map component

---
+++ src/components/Map/Map.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Map.css';
+
+function Map() {
+  return (
+    <div className="Map">
+      <header className="Map-header">
+        <p>
+          Edit <code>src/Map.js</code> and save to reload.
+        </p>
+        <a
+          className="Map-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Map;
